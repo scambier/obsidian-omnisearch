5602b1d8c858fd16de5691e0bf8765909c44a6bd
Simon Cambier <simon.cambier@protonmail.com>
2022-04-14 22:41:12 +0200

    Useless import

--- src/globals.ts
+++ src/globals.ts
@@ -1,5 +1,3 @@
-import { match } from 'assert'
-
 // Matches a wikiling that begins a string
 export const regexWikilink = /^!?\[\[(?<name>.+?)(\|(?<alias>.+?))?\]\]/
 export const regexLineSplit = /\r?\n|\r|((\.|\?|!)( |\r?\n|\r))/g
