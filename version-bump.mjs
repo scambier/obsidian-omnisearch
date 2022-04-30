0072f14ef65b8e422331248e58be74fd49aec285
Simon Cambier <simon.cambier@protonmail.com>
2022-04-30 21:32:24 +0200

    1.1.0

--- version-bump.mjs
+++ version-bump.mjs
@@ -3,10 +3,10 @@ import { readFileSync, writeFileSync } from 'fs'
 const targetVersion = process.env.npm_package_version
 
 // read minAppVersion from manifest.json and bump version to target version
-const manifest = JSON.parse(readFileSync('manifest.json', 'utf8'))
+const manifest = JSON.parse(readFileSync('manifest-beta.json', 'utf8'))
 const { minAppVersion } = manifest
 manifest.version = targetVersion
-writeFileSync('manifest.json', JSON.stringify(manifest, null, '\t'))
+writeFileSync('manifest-beta.json', JSON.stringify(manifest, null, '\t'))
 
 // update versions.json with target version and minAppVersion from manifest.json
 const versions = JSON.parse(readFileSync('versions.json', 'utf8'))
