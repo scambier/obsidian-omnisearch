cb84f109c01f2031cf8cae15d56b5db16bbcf2d8
Simon Cambier <simon.cambier@protonmail.com>
2022-05-02 18:36:33 +0200

    Fixed a crash

--- src/components/ResultItemVault.svelte
+++ src/components/ResultItemVault.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-import type { Query } from "src/query";
 import type { ResultNote } from "../globals"
 import { getMatches } from "../search"
 import { highlighter, makeExcerpt, stringsToRegex } from "../utils"
