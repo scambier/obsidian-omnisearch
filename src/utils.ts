69e062bc694e64e826f2a54a1582f274baa5d6a4
Simon Cambier <simon.cambier@protonmail.com>
2022-05-14 21:11:54 +0200

    Removed useless code

--- src/utils.ts
+++ src/utils.ts
@@ -61,19 +61,6 @@ export function stringsToRegex(strings: string[]): RegExp {
   return new RegExp(strings.map(s => `(${escapeRegex(s)})`).join('|'), 'gi')
 }
 
-export function replaceAll(
-  text: string,
-  terms: string[],
-  cb: (t: string) => string,
-): string {
-  terms.sort((a, b) => a.length - b.length)
-  const regs = terms.map(term => new RegExp(escapeRegex(term), 'gi'))
-  for (const reg of regs) {
-    text = text.replaceAll(reg, cb)
-  }
-  return text
-}
-
 export function extractHeadingsFromCache(
   cache: CachedMetadata,
   level: number,
