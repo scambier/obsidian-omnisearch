7797ff6769d0525d412cee7ebc2b9acd2a17343d
Simon Cambier <simon.cambier@protonmail.com>
2022-09-21 08:29:11 +0200

    Prettier for svelte

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,14 @@
 // prettier.config.js or .prettierrc.js
 module.exports = {
+  plugins: ['prettier-plugin-svelte'],
   trailingComma: 'es5',
   tabWidth: 2,
   semi: false,
   singleQuote: true,
   arrowParens: 'avoid',
-  bracketSameLine: true
+  bracketSameLine: true,
+  svelteStrictMode: true,
+  svelteBracketNewLine: false,
+  svelteAllowShorthand: true,
+  svelteIndentScriptAndStyle: true,
 }
