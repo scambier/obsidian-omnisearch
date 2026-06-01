07e8a7084dd112221c04e4e7a51784017915da7c
Dharya-dev <Dharya-dev@users.noreply.github.com>
2026-06-02 00:55:29 +0530

    fix(mobile): remove strict boolean check for newPane

--- src/tools/notes.ts
+++ src/tools/notes.ts
@@ -51,7 +51,7 @@ export async function openNote(
       await app.workspace.openLinkText(
         linkPath,
         '',
-        newLeaf ? 'split' : (newPane === true ? 'tab' : newPane)
+        newLeaf ? 'split' : (newPane ? 'tab' : newPane)
       )
       return
     }
@@ -95,13 +95,13 @@ export async function openNote(
         : undefined
 
     if (existingFile instanceof TFile) {
-      const leaf = app.workspace.getLeaf(newLeaf ? 'split' : (newPane === true ? 'tab' : newPane))
+      const leaf = app.workspace.getLeaf(newLeaf ? 'split' : (newPane ? 'tab' : newPane))
       await leaf.openFile(existingFile, { active: !newLeaf && !newPane })
     } else {
       await app.workspace.openLinkText(
         item.path,
         '',
-        newLeaf ? 'split' : (newPane === true ? 'tab' : newPane)
+        newLeaf ? 'split' : (newPane ? 'tab' : newPane)
       )
     }
   }
